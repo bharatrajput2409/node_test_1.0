311c52fc59a13f6a1d6294f48dad133556a9e9c5
Bharat singh shekhawat <58418023+bharatrajput2409@users.noreply.github.com>
2021-06-17 22:27:46 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,53 +1,51 @@
-try {
-    let data = [];
-    let header = [];
-    const sheets = file.SheetNames;
-    console.log(file.Sheets[file.SheetNames[0]]["H1"]);
-    let col = "";
-    let colcount = 0;
-    let flag = false;
-    let row = 0;
-    for (let i = 2; ; i++) {
-      if (file.Sheets[file.SheetNames[0]]["A" + i] !== undefined) {
-        row++;
-      } else {
-        break;
-      }
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+      zIndex:'999'
+    },
+  },
+}));
+
+export default function CustomizedSnackbars(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
     }
-    while (true) {
-      for (let i = 0; i < 26; i++) {
-        let c = col + String.fromCharCode("A".charCodeAt() + i);
-        if (file.Sheets[file.SheetNames[0]][c + "1"] === undefined) {
-          flag = true;
-          break;
-        } else {
-          colcount++;
-          header.push(c);
-        }
-      }
-      col = col + "A";
-      if (flag) {
-        break;
-      }
-    }
-    let r = 2;
-    while (row--) {
-      let c = colcount;
-      let rowdata = {};
-      header.forEach((col) => {
-        console.log(file.Sheets[file.SheetNames[0]][col + "1"].v, col + r);
-        if (file.Sheets[file.SheetNames[0]][col + r] !== undefined) {
-          rowdata[file.Sheets[file.SheetNames[0]][col + "1"].v] =
-            file.Sheets[file.SheetNames[0]][col + r].v;
-        } else {
-          rowdata[file.Sheets[file.SheetNames[0]][col + "1"].v] = "";
-        }
-      });
-      data.push(rowdata);
-      r++;
-    }
-    return res.send(data);
-  } catch (err) {
-    console.log(err);
-    res.send(rest_data);
-  }
+
+    setOpen(false);
+    props.setOpen(false);
+  };
+  const vertical = "bottom";
+  const horizontal = "left";
+  return (
+		<div className={classes.root}>
+			<Snackbar
+				open={open}
+				autoHideDuration={6000}
+				onClose={handleClose}
+				anchorOrigin={{ vertical, horizontal }}
+				key={vertical + horizontal}
+        style={{zIndex:'10000'}}
+			>
+				<Alert onClose={handleClose} severity={props.severity}>
+					{props.message}
+				</Alert>
+			</Snackbar>
+		</div>
+	);
+}
